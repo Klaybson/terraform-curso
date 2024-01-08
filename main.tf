7192434ceefa9d8486c082f4d519a8000755c66e
klaybson <klaybson@gmail.com>
2024-01-08 02:01:16 +0000

    Instancia EC2

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@
 
 ########################CODIGO#####################
 terraform {
-    required_version = ">= 1.1.0"
-    required_providers {
-        aws = ">= 4.0"
-    }
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = ">= 4.0"
+  }
 }
 
 
@@ -38,4 +38,31 @@ provider "aws" {
   profile                  = "default"
   shared_credentials_files = ["~/.aws/credentials"]
   region                   = "us-east-1"
+}
+
+##Exemplo basico
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
